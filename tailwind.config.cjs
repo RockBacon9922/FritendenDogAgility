a39a43374400a50cf3a67f42fbc1a10c3e9d890e
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2023-02-22 23:32:47 +0000

    Setup colours

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        FDA: '#165B1C',
+        FDA: "#165B1C",
+        FDA0: "#4D5359", // zinc-60
+        FDA1: "#114B5F", // cyan-900
+        FDA2: "#1A936F", // Emerald-600
+        FDA3: "#88D498", // green-300
+        FDA4: "#C6DABF", // stone-300
+        FDA5: "#F3E9D2", // orange-100
       },
     },
   },
