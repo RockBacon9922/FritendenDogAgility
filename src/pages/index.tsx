199a28b628458e8b9a7918e56286ba5535ddc29c
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2023-02-23 00:15:57 +0000

    setup the index page to a working state. The old index page is currently acting as the dashboard.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,53 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
+import { useRouter } from "next/router";
 import Image from "next/image";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, useSession, getProviders } from "next-auth/react";
 import FDALogo from "../../Images/FDALogo.svg";
 
 import { api } from "../utils/api";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  // if signed in, redirect to the dashboard
+  const { data: session, status } = useSession();
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+  if (session) {
+    void router.push("/oldindex");
+    return <div>Redirecting...</div>;
+  }
+
+  // get the providers
+  const providers = {
+    facebook: { name: "Facebook", id: "facebook" },
+  };
+
   return (
     <>
       <Head>
         <title>FDA League</title>
         <meta name="description" content="The dog agility league " />
       </Head>
-      <div className="bg-FDA0">
-        <div className="paddock h-full h-screen w-full flex-col bg-blend-saturation">
+      <div className="grid grid-cols-3 bg-FDA5">
+        <div className="place-self-center ">
+          {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
           <Image src={FDALogo} alt="FDA Logo" width={500} height={500} />
+        </div>
+        <div className="paddock col-span-2">
+          <div className="flex h-screen w-full flex-row items-center justify-end bg-blend-saturation">
+            <div className="mr-3 rounded-md bg-FDA0 p-3 text-white">
+              {Object.values(providers).map((provider) => (
+                <div key={provider.name}>
+                  {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+                  <button onClick={() => signIn(provider.id)}>
+                    Sign in with {provider.name}
+                  </button>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </>
