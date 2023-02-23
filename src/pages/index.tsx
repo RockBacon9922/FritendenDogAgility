bda9aaae2eba75f98178069ee1600160afa6284d
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2023-02-23 00:36:34 +0000

    Starting to make mobile friendly

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,13 +30,13 @@ const Home: NextPage = () => {
         <title>FDA League</title>
         <meta name="description" content="The dog agility league " />
       </Head>
-      <div className="grid grid-cols-3 bg-FDA5">
+      <div className="h-screen w-full grid-cols-4 bg-FDA5 md:grid">
         <div className="place-self-center ">
           {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
           <Image src={FDALogo} alt="FDA Logo" width={500} height={500} />
         </div>
-        <div className="paddock col-span-2">
-          <div className="flex h-screen w-full flex-row items-center justify-end bg-blend-saturation">
+        <div className="paddock col-span-3 hidden md:block">
+          <div className="flex flex-row items-center justify-end">
             <div className="mr-3 rounded-md bg-FDA0 p-3 text-white">
               {Object.values(providers).map((provider) => (
                 <div key={provider.name}>
