b1fcb16a050e775add2cdbfb3c776dee99b63496
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2023-02-25 13:38:43 +0000

    Half built the reCalculateScores api

--- src/pages/api/reCalculateScores.tsx
+++ src/pages/api/reCalculateScores.tsx
@@ -1,17 +1,20 @@
 import { PrismaClient } from "@prisma/client";
-import { NextApiRequest, NextApiResponse } from "next";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const prisma = new PrismaClient()
-    // find every dog
-    // find every event for that dog
-    const events = prisma.eventEntry.findMany({
-        where: {
-            verified: true,
-            updatedAt: {
+// get current date
 
-            }
-        },
+const handler = async () => {
+  const prisma = new PrismaClient();
+  // find every dog
+  // find every event for that dog
+  const events = await prisma.eventEntry.findMany({
+    where: {
+      verified: true,
+      updatedAt: {
+        gt: new Date(new Date().getTime() - 5 * 60 * 1000),
+      },
+    },
+  });
+  console.log(events);
+};
 
-    })
-}
+export default handler;
