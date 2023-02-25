50fb736110a0e60d94148159ae1c53747c369c0b
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2023-02-25 16:50:23 +0000

    Dashboard half complete

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,29 @@
-import { type NextPage } from "next";
+import { type GetServerSideProps, type NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import { signIn, useSession } from "next-auth/react";
-import FDALogo from "../../Images/FDALogo.svg";
+import { useSession, signOut } from "next-auth/react";
+import add from "../../Images/add.svg";
+import { getServerAuthSession } from "../server/auth";
+import Link from "next/link";
+import signOutIcon from "../../Images/signOut.svg";
 
-const Dashboard: NextPage = () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getServerAuthSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+};
+
+const Dashboard: NextPage = ({}) => {
   const router = useRouter();
   // if signed in, redirect to the dashboard
   const { data: session, status } = useSession();
@@ -25,7 +43,8 @@ const Dashboard: NextPage = () => {
       </Head>
       <div className="grid h-screen w-full grid-rows-4 items-center bg-gradient-to-t from-emerald-400 to-teal-200 md:grid-cols-4 md:grid-rows-1">
         <News />
-        {/* <LeagueTable /> */}
+        <LeagueTable />
+        <Menu />
       </div>
     </>
   );
@@ -35,7 +54,7 @@ export default Dashboard;
 
 const News = () => {
   return (
-    <div className="mx-3 h-[90%] rounded-lg bg-slate-300 bg-opacity-50 p-3 text-slate-600">
+    <div className="card">
       <h2 className="text-center text-xl font-extrabold tracking-tight ">
         The News
       </h2>
@@ -44,9 +63,54 @@ const News = () => {
 };
 
 const LeagueTable = () => {
-  return <div className="row-span-2 md:col-span-2"></div>;
+  return <div className="card row-span-2 md:col-span-2">i have some text</div>;
 };
 
 const Menu = () => {
-  return <div></div>;
+  return (
+    <ul className="card">
+      {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
+      <MenuItem link={"/addEvent"} icon={add} text="Record An Event" />
+      <SignOutComponent />
+    </ul>
+  );
 };
+
+const MenuItem = ({
+  link,
+  icon,
+  text,
+}: {
+  link: string;
+  icon: string;
+  text: string;
+}) => {
+  return (
+    <li className="">
+      <Link
+        href={link}
+        className="flex flex-row items-center gap-5 font-bold"
+      >
+        {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
+        <Image src={icon} alt="text" width={50} height={50} />
+        <h3>{text}</h3>
+      </Link>
+    </li>
+  );
+};
+
+const SignOutComponent = () => {
+  return (
+    <li className="">
+      <button
+        onClick={() => void signOut()}
+        className="flex flex-row items-center gap-5 font-bold"
+      >
+        {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
+        <Image src={signOutIcon} alt="text" width={50} height={50} />
+        <h3>Logout</h3>
+      </button>
+    </li>
+  );
+};
+
