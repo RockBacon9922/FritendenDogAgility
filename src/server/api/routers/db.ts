74aa9d127ca435aa7784b9d1411d4d1193f0509b
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2023-02-25 20:31:47 +0000

    Created an api procedure addEvent: allows the user to add an event to the database.

--- src/server/api/routers/db.ts
+++ src/server/api/routers/db.ts
@@ -22,4 +22,28 @@ export const dbRouter = createTRPCRouter({
     await prisma.$disconnect()
     return news
   }),
+  addEvent: protectedProcedure.input(
+    z.object({
+      dogId: z.string(),
+      grade: z.number(),
+      height: z.number(),
+      userId: z.string(),
+      dateOfEvent: z.date(),
+      event: z.string(),
+      points: z.number(),
+      })).mutation(async ({ input }) => {
+        const event = await prisma.eventEntry.create({
+          data: {
+            dogId: input.dogId,
+            grade: input.grade,
+            height: input.height,
+            userId: input.userId,
+            dateOfEvent: input.dateOfEvent,
+            event: input.event,
+            points: input.points,
+          },
+        });
+        await prisma.$disconnect()
+        return event;
+      }),
 });
