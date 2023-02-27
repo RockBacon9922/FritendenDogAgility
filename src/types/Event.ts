b22921b4cbf6a04c686e28616665a27a6cb8def1
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2023-02-27 23:20:10 +0000

    Created events type

---
+++ src/types/Event.ts
@@ -0,0 +1,13 @@
+type Event = {
+  dogId: string;
+  grade: number;
+  height: string;
+  userId: string;
+  eventName: string;
+  eventType: string;
+  dateOfEvent: string;
+  league: string;
+  points: number;
+};
+
+export default Event;
