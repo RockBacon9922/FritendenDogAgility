a8ae9d6c81c6df15f0674da68bc6f9cd80083ca2
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2023-03-01 21:40:09 +0000

    Fix React issues with path variable names

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -111,9 +111,9 @@ const CookieAlert = () => {
           className="h-6 w-6 flex-shrink-0 stroke-info"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
           ></path>
         </svg>
