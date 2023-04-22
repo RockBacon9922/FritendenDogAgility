e50efb2423f12d2ba218d987dae35d1846b530fc
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2023-04-22 14:42:08 +0100

    Sentry (#18)
    * Sentry test
    
    * added sentry clirc files to encrypted env
    
    * Added profiling to serverless functions

---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+import { ProfilingIntegration } from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: "https://00f288484fe8493fbb73365adabf2dfa@o4504764919185408.ingest.sentry.io/4505057890074624",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+  profilesSampleRate: 1,
+  integrations: [new ProfilingIntegration()],
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
