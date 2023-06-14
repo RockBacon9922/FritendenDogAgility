d1dc50ed71779899dbbf45edcd9bc3bc569bb20b
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2023-06-14 11:57:24 +0100

    Add privicy policy

---
+++ src/pages/privicy.tsx
@@ -0,0 +1,154 @@
+import React from "react";
+
+const PrivacyPolicy: React.FC = () => {
+  return (
+    <>
+      <h1>Privacy Policy</h1>
+
+      <p>
+        <em>Effective Date: 14 Jul 23</em>
+      </p>
+
+      <p>
+        At Frittenden Dog Agility, we value the privacy of our members and
+        understand the importance of protecting their personal information. This
+        Privacy Policy outlines how we collect, use, and safeguard the data
+        provided to us through our website and related services. By using our
+        website, you consent to the terms of this Privacy Policy.
+      </p>
+
+      <h2>1. Information We Collect</h2>
+
+      <p>
+        When you use our website to input your competition results, we may
+        collect the following information:
+      </p>
+
+      <ul>
+        <li>Name of the handler</li>
+        <li>Dogs names</li>
+        <li>
+          Details of each event, such as competition date, location, and results
+        </li>
+      </ul>
+
+      <h2>2. Use of Information</h2>
+
+      <p>We use the collected information for the following purposes:</p>
+
+      <ul>
+        <li>To maintain records of competition results for our members.</li>
+        <li>
+          To provide personalized services and improve the user experience.
+        </li>
+        <li>
+          To communicate with our members regarding competition updates, events,
+          and relevant club information.
+        </li>
+        <li>
+          To analyze trends, monitor usage patterns, and improve our website and
+          services.
+        </li>
+        <li>
+          To comply with legal obligations or respond to lawful requests from
+          authorities.
+        </li>
+      </ul>
+
+      <h2>3. Data Security</h2>
+
+      <p>
+        We implement appropriate security measures to protect the information
+        provided to us. However, please note that no method of transmission or
+        electronic storage is completely secure. While we strive to protect your
+        personal information, we cannot guarantee its absolute security.
+      </p>
+
+      <h2>4. Data Retention</h2>
+
+      <p>
+        We retain the collected information for as long as necessary to fulfil
+        the purposes outlined in this Privacy Policy. When information is no
+        longer needed, we will securely dispose of it in accordance with
+        applicable laws and regulations.
+      </p>
+
+      <h2>5. Sharing of Information</h2>
+
+      <p>
+        We do not sell, trade, or otherwise transfer your personal information
+        to third parties without your explicit consent, except as described
+        below:
+      </p>
+
+      <ul>
+        <li>
+          We may share your information with authorized third-party service
+          providers who assist us in operating our website and providing our
+          services.
+        </li>
+        <li>
+          We may share your information if required by law, court order, or
+          governmental authority.
+        </li>
+        <li>
+          We may share aggregated, non-personal information for statistical and
+          analytical purposes.
+        </li>
+      </ul>
+
+      <h2>6. Your Rights</h2>
+
+      <p>
+        You have certain rights regarding the personal information we hold about
+        you. You may:
+      </p>
+
+      <ul>
+        <li>Access and review your personal information.</li>
+        <li>Correct any inaccuracies in your personal information.</li>
+        <li>
+          Request the deletion of your personal information, subject to our
+          legal obligations.
+        </li>
+        <li>Object to the processing of your personal information.</li>
+        <li>
+          Withdraw your consent for the collection, use, or disclosure of your
+          personal information.
+        </li>
+      </ul>
+
+      <p>
+        To exercise your rights, please contact us using the information
+        provided in Section 8.
+      </p>
+
+      <h2>7. Third-Party Links</h2>
+
+      <p>
+        Our website may contain links to third-party websites. We are not
+        responsible for the privacy practices or content of such websites. We
+        encourage you to review the privacy policies of these third-party
+        websites before providing any personal information.
+      </p>
+
+      <h2>8. Contact Us</h2>
+
+      <p>
+        If you have any questions, concerns, or requests regarding this Privacy
+        Policy or the handling of your personal information, please contact us
+        at:
+      </p>
+
+      <address>
+        Frittenden Dog Agility
+        <br />
+        South View Lodge Frittenden Road Frittenden Kent
+        <br />
+        <a href="https://frittendendogagility.com">frittendendogagility.com</a>
+      </address>
+    </>
+  );
+};
+
+export default PrivacyPolicy;
